3627b5ef9c63cf3137857970985bd119a864ce0a
Tomasz <Tomasz5792@hotmail.co.uk>
2025-03-31 15:48:51 +0100

    deleted to do it again

--- Task 3/main_fizzbuzz.py
+++ Task 3/main_fizzbuzz.py
@@ -5,19 +5,3 @@ Multiples of 5 with "Buzz"
 Multiples of both 3 and 5 with "FizzBuzz"
 """
 
-def fizzbuzz(number: int) -> str:
-    
-    if number % 3 == 0 and number % 5 == 0:
-        return "FizzBuzz"
-
-    if number % 3 == 0:
-        return "Fizz"
-    
-    if number % 5 == 0:
-        return "Buzz"   
-    
-    return str(number)
-    
-
-
-print(fizzbuzz(3))
--- Task 3/test_fizzbuzz.py
+++ Task 3/test_fizzbuzz.py
@@ -5,15 +5,3 @@ Multiples of 5 with "Buzz"
 Multiples of both 3 and 5 with "FizzBuzz"
 """
 
-from main_fizzbuzz import fizzbuzz
-
-def test_fizzbuzz():
-    assert fizzbuzz(1) == "1"
-    assert fizzbuzz(2) == "2"
-    assert fizzbuzz(3) == "Fizz"
-    assert fizzbuzz(4) == "4"
-    assert fizzbuzz(5) == "Buzz"
-    assert fizzbuzz(6) == "Fizz"
-    assert fizzbuzz(7) == "7"
-    assert fizzbuzz(8) == "8"
-    assert fizzbuzz(15) == "FizzBuzz"
