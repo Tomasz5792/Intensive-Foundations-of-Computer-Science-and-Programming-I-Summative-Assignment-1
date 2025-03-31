1c526d50becd5bbb1b587706d75d7103f0eab299
Tomasz <Tomasz5792@hotmail.co.uk>
2025-03-31 15:32:56 +0100

    made more tests

--- Task 3/test_fizzbuzz.py
+++ Task 3/test_fizzbuzz.py
@@ -9,3 +9,11 @@ from main_fizzbuzz import fizzbuzz
 
 def test_fizzbuzz():
     assert fizzbuzz(1) == "1"
+    assert fizzbuzz(2) == "2"
+    assert fizzbuzz(3) == "Fizz"
+    assert fizzbuzz(4) == "4"
+    assert fizzbuzz(5) == "Buzz"
+    assert fizzbuzz(6) == "6"
+    assert fizzbuzz(7) == "7"
+    assert fizzbuzz(8) == "8"
+    assert fizzbuzz(15) == "FizzBuzz"
