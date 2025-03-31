23e35e1fff4a23902e8702a5cc21a9de6f945f42
Tomasz <Tomasz5792@hotmail.co.uk>
2025-03-31 15:58:27 +0100

    added tests and action

--- Task 3/test_fizzbuzz.py
+++ Task 3/test_fizzbuzz.py
@@ -5,3 +5,19 @@ Multiples of 5 with "Buzz"
 Multiples of both 3 and 5 with "FizzBuzz"
 """
 
+from main_fizzbuzz import fizzbuzz
+
+def test_fizzbuzz_numbers_correct():
+    assert fizzbuzz(1) == "1"
+    assert fizzbuzz(2) == "2"
+    assert fizzbuzz(4) == "4"
+    assert fizzbuzz(7) == "7"
+    assert fizzbuzz(8) == "8"
+    assert fizzbuzz(9) == "9"
+    assert fizzbuzz(10) == "10"
+    
+def test_fizzbuzz_numbers_notcorrect():
+    assert fizzbuzz(3) != "3"
+    assert fizzbuzz(15) != "15"
+    assert fizzbuzz(5) != "5"
+    assert fizzbuzz(6) != "6"
