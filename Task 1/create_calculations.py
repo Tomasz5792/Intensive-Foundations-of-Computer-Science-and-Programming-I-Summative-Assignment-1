ef86f4ee7d838c8144032d9e292a074e0628251e
Tomasz <Tomasz5792@hotmail.co.uk>
2025-04-06 15:56:10 +0100

    Saving before creating data

---
+++ Task 1/create_calculations.py
@@ -0,0 +1,16 @@
+
+import random
+
+data = {
+    "question1":{"question_text":"question_text","answer_text":"answer_text","answer_correct?":"not_answered"
+                 ,"answer_text_1":"answer_text_1","is_correct_answer_1":False
+                 ,"answer_text_2":"answer_text_2","is_correct_answer_2":False
+                 ,"answer_text_3":"answer_text_3","is_correct_answer_3":False
+                 ,"answer_text_4":"answer_text_4","is_correct_answer_4":False
+                 ,"answer_text_5":"answer_text_5","is_correct_answer_5":False
+                 ,"answer_text_6":"answer_text_6","is_correct_answer_6":False
+                 },
+}
+
+def create_questions():
+    return
--- Task 1/user_interface.py
+++ Task 1/user_interface.py
@@ -1,33 +1,40 @@
 import tkinter as tk
 
+
+
 root = tk.Tk()
 
 def create_gui():
     """
     Create a simple GUI
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Raises:
+        None
     """
     root.title("Title")
 
     #row 1
-    create_lable()
+    create_label(row=0)
 
     #row 2
-    button = create_button(1,1)
-    button2 = create_button(1,2)
-    button3 = create_button(1,3)
+    button = create_button(row=1,column=1)
+    button2 = create_button(row=1,column=2)
+    button3 = create_button(row=1,column=3)
 
     #row 3
-    button4 = create_button(2,1)
-    button5 = create_button(2,2)
-    button6 = create_button(2,3)
+    button = create_button(row=2,column=1)
+    button2 = create_button(row=2,column=2)
+    button3 = create_button(row=2,column=3)
 
     #row 4
-    button7 = create_button(3,1)
-    button8 = create_button(3,2)
-    button9 = create_button(3,3)
-
-    #row 5
-
+    create_label(row=3,columnspan=2)
+    button3 = create_button(row=3,column=3,height=1)
 
     root.grid_rowconfigure(0, weight=1)
     root.grid_rowconfigure(1, weight=1)
@@ -42,17 +49,49 @@ def create_gui():
 
     root.mainloop()
 
-def create_lable():
-    label = tk.Label(root, text="Click a button below!", font=("Arial", 14))
-    label.grid(row=0, column=1, columnspan=3, pady=10, sticky="n")
+def create_label(row: int,columnspan: int=3):
+    """
+    Create lable for gui
 
-def create_button(row: int, column: int):
-    button = tk.Button(root, text="Click me", width=20, height=5)
+    Args:
+        row: (int): The row the label is created on.
+        columnspan (int, optional): The amount of columns the label spans. Defaults to 3.
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
+    label = tk.Label(root, text="Click a button below!", font=("Arial", 14))
+    label.grid(row=row, column=1, columnspan=columnspan, pady=10, sticky="n")
+
+def create_button(row: int, column: int, width: int=20, height: int=5):
+    """
+    Create button for gui
+
+    Args:
+        row: (int): The row the button is created on.
+        column: (int): The column the button is created on.
+        width: (int, optional): The width of the button.  Defaults to 20.
+        height: (int, optional): The height of the button.  Defaults to 5.
+
+    Returns:
+        tk.Button: The created button widget.
+
+    Raises:
+        None
+    """
+    button = tk.Button(root, text="Click me", width=width, height=height)
     button.grid(row=row, column=column, padx=5, pady=5, sticky="nsew")
-    button.bind("<Button-1>", handle_button_press)
+    button.bind("<Button-1>", lambda event: handle_button_press(event, row, column))
     return button
 
-def handle_button_press(event):
-    print("Button pressed!")
+def handle_button_press(event, row, column):
+    """
+    Handles button press event
+    """
+    print(f"Button row: {row} coumn: {column} was pressed.")
+
 
 create_gui()
